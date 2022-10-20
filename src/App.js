46bc2057a0568b4837191656af1e6c1ee3ecd5a9
R <ryansereno@protonmail.com>
2022-10-20 17:33:34 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,98 @@
+import { Fragment } from "react";
+import "./App.css";
+import FadeInSection from "./components/FadeInSection";
+
+function App() {
+  const frontendText = "Frontend";
+  let frontendHtml = [];
+  for (let i = 0; i < frontendText.length; i++) {
+    frontendHtml.push(
+      <span key={i} className="header-letter">
+        {frontendText[i]}
+      </span>
+    );
+  }
+  return (
+    <Fragment>
+      <header>
+        <h1>Ryan Sereno</h1>
+      </header>
+      <div className="App">
+        <h2>
+          {frontendHtml}&nbsp;
+          <br />
+          Engineer
+        </h2>
+      </div>
+      <FadeInSection>
+        <div className="about">
+          <p>
+            Organic chemist turned software engineer. <br />
+            Building beautiful, intuitive software.
+            <br />
+            Specialized in React development and UI design.
+          </p>
+        </div>
+      </FadeInSection>
+      <FadeInSection>
+        <section>
+          <h3>Skillset</h3>
+          <div className="skill-section">
+            <div className="skill-row">
+              <div className="skill">
+                <img src="https://skills.thijs.gg/icons?i=react" />
+                <p>React</p>
+              </div>
+              <div className="skill">
+                <img src="https://skills.thijs.gg/icons?i=javascript" />
+                <p>Javascript</p>
+              </div>
+              <div className="skill">
+                <img src="https://skills.thijs.gg/icons?i=typescript" />
+                <p>Typescript</p>
+              </div>
+              <div className="skill">
+                <img src="https://skills.thijs.gg/icons?i=css" />
+                <p>CSS</p>
+              </div>
+            </div>
+            <div className="skill-row">
+              <div className="skill">
+                <img src="https://skills.thijs.gg/icons?i=nextjs" />
+                <p>Next.js</p>
+              </div>
+              <div className="skill">
+                <img src="https://skills.thijs.gg/icons?i=nodejs" />
+                <p>Node</p>
+              </div>
+              <div className="skill">
+                <img src="https://skills.thijs.gg/icons?i=mongodb" />
+                <p>MongoDB</p>
+              </div>
+              <div className="skill">
+                <img src="https://skills.thijs.gg/icons?i=mysql" />
+                <p>MySql</p>
+              </div>
+              <div className="skill">
+                <img src="https://skills.thijs.gg/icons?i=ai" />
+                <p>Illustrator</p>
+              </div>
+            </div>
+          </div>
+        </section>
+      </FadeInSection>
+      <FadeInSection>
+        <section>
+          <h3>Work</h3>
+          <div className="portfolio">
+            <div className="project"></div>
+            <div className="project"></div>
+            <div className="project"></div>
+          </div>
+        </section>
+      </FadeInSection>
+    </Fragment>
+  );
+}
+
+export default App;
